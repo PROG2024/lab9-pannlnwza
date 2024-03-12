2da4ad2a9766c43cc50c834d207cc4e0501f48bd
Pattapon Gowanit <iampattapon@gmail.com>
2024-03-12 16:18:45 +0700

    Problem 1

--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,31 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
 
-# TODO write 3 tests as described above
+
+class TestCircle(unittest.TestCase):
+    def setUp(self):
+        """Creates circle before test."""
+        self.circle = Circle(1)
+        self.circle1 = Circle(2)
+        self.circle_edge_case = Circle(0)
+
+    def test_add_area(self):
+        """Test for add_area using typical values."""
+        expected_radius = (self.circle.get_radius()**2 + self.circle1.get_radius()**2)**0.5
+        self.assertEqual(expected_radius, self.circle.add_area(self.circle1).radius)
+        expected_area = Circle(expected_radius).get_area()
+        self.assertEqual(expected_area, self.circle.add_area(self.circle1).get_area())
+
+    def test_add_area_edge_case(self):
+        """Test for add_area for an "edge case" where one circle has radius 0."""
+        expected_radius = (self.circle_edge_case.get_radius() ** 2 + self.circle.get_radius() ** 2) ** 0.5
+        self.assertEqual(expected_radius, self.circle_edge_case.add_area(self.circle).radius)
+        expected_area = Circle(expected_radius).get_area()
+        self.assertEqual(expected_area, self.circle_edge_case.add_area(self.circle).get_area())
+
+    def test_negative_radius(self):
+        """Test that circle constructor raises exception of radius is negative."""
+        with self.assertRaises(ValueError):
+            Circle(-1)
